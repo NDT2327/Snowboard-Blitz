02af1fb050ae28c4b9931d1caba5ba6b77f56639
Phước Thành <thanhgithub123@gmail.com>
2025-03-02 22:26:53 +0700

    Xong giao dien Menu, Over , Control

---
+++ Assets/Scripts/Static-Action/Menu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    // trở về Menu 
+    public void GoToMenu() {
+        SceneManager.LoadScene(3);
+    }
+
+// thoát game
+    public void Quit() {
+        Application.Quit();
+    }
+
+    // bắt đầu chơi - Chơi lại
+    public void Play() {
+        SceneManager.LoadScene(0);
+    }
+
+    // Bảng điều khiển
+    public void Control()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+
+}
