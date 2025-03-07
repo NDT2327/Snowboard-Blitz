81c7049ea7181724775fe2e899c10436bfc87d8c
Phạm Cường <phamcuongtran222@gmail.com>
2025-03-07 11:22:44 +0700

    add LoadScene

--- Assets/EndFlagScript.cs
+++ Assets/EndFlagScript.cs
@@ -3,17 +3,30 @@ using UnityEngine.SceneManagement; // Để load màn mới
 
 public class EndFlagScript : MonoBehaviour
 {
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    if (collision.gameObject.CompareTag("Player")) // Kiểm tra nếu nhân vật chạm vào
-    //    {
-    //        LoadNextLevel();
-    //    }
-    //}
 
-    //void LoadNextLevel()
-    //{
-    //    int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; // Lấy màn hiện tại
-    //    SceneManager.LoadScene(currentSceneIndex + 1); // Load màn tiếp theo
-    //}
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) // Kiểm tra nếu nhân vật chạm vào
+        {
+            Game1Event game1Event = FindFirstObjectByType<Game1Event>();
+            if (game1Event != null)
+            {
+                game1Event.LoadNextLevel(); // Gọi hàm LoadNextLevel()
+            }
+            else
+            {
+                Debug.LogError("Không tìm thấy Game1Event trong scene!");
+            }
+        }
+    }
+
 }
